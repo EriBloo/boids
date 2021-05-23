5b59b23db6b32b0949f56f455aa9a27047371b4c
eriBloo <lprzybyslawski@gmail.com>
2021-05-23 21:28:49 +0200

    refactor: Small settings changes

--- src/scripts/settings.ts
+++ src/scripts/settings.ts
@@ -1,13 +1,13 @@
 export const settings = {
   numBoids: 100, // number of boids
-  breakFactor: 0.2, // improves performance at the cost of calculations accuracy: breakFactor * numBoids = boid won't check more boids than this
+  breakFactor: 0.15, // improves performance at the cost of calculations accuracy: breakFactor * numBoids = boid won't check more flockmates than this
   viewRadius: 100, // boids in this radius will be included in calculations
   avoidRadius: 40, // boids in this radius will be avoided
   viewAngle: 120, // angle to look for boids (NOT IMPLEMENTED)
-  minSpeed: 4, // min speed of boid
-  maxSpeed: 6, // max speed of boid
+  minSpeed: 6, // min speed of boid
+  maxSpeed: 10, // max speed of boid
   maxSteer: 0.5, // max turn speed of boid
   separationWeight: 1, // weight to adjust separation
   alignmentWeight: 1, // weight to adjust alignment
-  cohesionWeight: 1, // weight to adjust cohesion
+  cohesionWeight: 0.6, // weight to adjust cohesion
 };
